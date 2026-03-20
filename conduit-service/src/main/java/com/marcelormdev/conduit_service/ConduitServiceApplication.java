6a50688b82c0b856cda2779aa8c6efa60cee0ef8
marcelorm-dev <marcelormdev@gmail.com>
2026-03-20 16:21:05 -0300

    create the start of the project conduit-service

---
+++ conduit-service/src/main/java/com/marcelormdev/conduit_service/ConduitServiceApplication.java
@@ -0,0 +1,13 @@
+package com.marcelormdev.conduit_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConduitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConduitServiceApplication.class, args);
+	}
+
+}
