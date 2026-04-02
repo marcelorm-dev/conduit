44075ae44d90d6176139925021afa10530810b11
marcelorm-dev <marcelormdev@gmail.com>
2026-04-02 20:34:11 -0300

    Format UserController class applying editor settings

--- conduit-service/src/main/java/com/marcelormdev/conduit_service/user/UserController.java
+++ conduit-service/src/main/java/com/marcelormdev/conduit_service/user/UserController.java
@@ -21,73 +21,73 @@ import com.marcelormdev.conduit_service.http.JwtAuthorizationHeader;
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
 
-  private final UserService userService;
+    private final UserService userService;
 
-  UserController(UserService userService) {
-    this.userService = userService;
-  }
-
-  private record BodyResponse(UserResponse user) {
-
-    private record UserResponse(String email, String username, String bio, String image, String token) {
+    UserController(UserService userService) {
+        this.userService = userService;
     }
 
-    static BodyResponse of(UserDTO userDTO) {
-      UserResponse userResponse = new UserResponse(
-          userDTO.email(),
-          userDTO.username(),
-          userDTO.bio(),
-          userDTO.image(),
-          userDTO.token());
+    private record BodyResponse(UserResponse user) {
 
-      return new BodyResponse(userResponse);
+        private record UserResponse(String email, String username, String bio, String image, String token) {
+        }
+
+        static BodyResponse of(UserDTO userDTO) {
+            UserResponse userResponse = new UserResponse(
+                    userDTO.email(),
+                    userDTO.username(),
+                    userDTO.bio(),
+                    userDTO.image(),
+                    userDTO.token());
+
+            return new BodyResponse(userResponse);
+        }
+
     }
 
-  }
+    @GetMapping("/api/user")
+    public BodyResponse currentUser(@RequestHeader HttpHeaders headers) {
+        String token = new JwtAuthorizationHeader(headers).getToken();
+        UserDTO currentUser = userService.currentUser(token);
 
-  @GetMapping("/api/user")
-  public BodyResponse currentUser(@RequestHeader HttpHeaders headers) {
-    String token = new JwtAuthorizationHeader(headers).getToken();
-    UserDTO currentUser = userService.currentUser(token);
+        return BodyResponse.of(currentUser);
+    }
 
-    return BodyResponse.of(currentUser);
-  }
+    @PostMapping("/api/users/login")
+    public BodyResponse authenticate(@RequestBody Map<String, Map<String, String>> body) {
+        UserDTO requestUser = new UserDTO(body.get("user"));
+        UserDTO loggedUser = userService.login(requestUser.email(), requestUser.password());
 
-  @PostMapping("/api/users/login")
-  public BodyResponse authenticate(@RequestBody Map<String, Map<String, String>> body) {
-    UserDTO requestUser = new UserDTO(body.get("user"));
-    UserDTO loggedUser = userService.login(requestUser.email(), requestUser.password());
+        return BodyResponse.of(loggedUser);
+    }
 
-    return BodyResponse.of(loggedUser);
-  }
+    @PostMapping("/api/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    public BodyResponse register(@RequestBody Map<String, Map<String, String>> body) {
+        UserDTO registeredUser = userService.register(new UserDTO(body.get("user")));
 
-  @PostMapping("/api/users")
-  @ResponseStatus(HttpStatus.CREATED)
-  public BodyResponse register(@RequestBody Map<String, Map<String, String>> body) {
-    UserDTO registeredUser = userService.register(new UserDTO(body.get("user")));
+        return BodyResponse.of(registeredUser);
+    }
 
-    return BodyResponse.of(registeredUser);
-  }
+    @PutMapping("/api/user")
+    public BodyResponse update(@RequestHeader HttpHeaders headers, @RequestBody Map<String, Map<String, String>> body) {
+        String token = new JwtAuthorizationHeader(headers).getToken();
+        UserDTO updatedUser = userService.update(token, new UserDTO(body.get("user")));
 
-  @PutMapping("/api/user")
-  public BodyResponse update(@RequestHeader HttpHeaders headers, @RequestBody Map<String, Map<String, String>> body) {
-    String token = new JwtAuthorizationHeader(headers).getToken();
-    UserDTO updatedUser = userService.update(token, new UserDTO(body.get("user")));
+        return BodyResponse.of(updatedUser);
+    }
 
-    return BodyResponse.of(updatedUser);
-  }
+    @GetMapping("/api/users")
+    public List<BodyResponse> getAllUsers(@RequestHeader HttpHeaders headers) {
+        String token = new JwtAuthorizationHeader(headers).getToken();
+        return userService.getAllUsers(token).stream().map(BodyResponse::of).toList();
+    }
 
-  @GetMapping("/api/users")
-  public List<BodyResponse> getAllUsers(@RequestHeader HttpHeaders headers) {
-    String token = new JwtAuthorizationHeader(headers).getToken();
-    return userService.getAllUsers(token).stream().map(BodyResponse::of).toList();
-  }
+    @PutMapping("/api/token")
+    public BodyResponse renewToken(@RequestBody Map<String, Map<String, String>> body) {
+        UserDTO user = userService.renewToken(body.get("user").get("email"));
 
-  @PutMapping("/api/token")
-  public BodyResponse renewToken(@RequestBody Map<String, Map<String, String>> body) {
-    UserDTO user = userService.renewToken(body.get("user").get("email"));
-
-    return BodyResponse.of(user);
-  }
+        return BodyResponse.of(user);
+    }
 
 }
